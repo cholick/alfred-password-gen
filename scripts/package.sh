a43947ff5d2d1d2beca05ad3233860fa52bef2fe
Matt Cholick <cholick@gmail.com>
2024-08-27 10:45:08 -0700

    Create script for packaging

---
+++ scripts/package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "${script_dir}"/..
+
+mkdir -p /tmp/alfred-password-gen
+mkdir -p /tmp/alfred-password-gen/dictionary
+
+cp workflow/icon.png /tmp/alfred-password-gen
+cp workflow/info.plist /tmp/alfred-password-gen
+
+cp generate.py /tmp/alfred-password-gen
+cp dictionary/processed.py /tmp/alfred-password-gen/dictionary
+
+version=$(cat workflow/version)
+sed -i '' "s/VERSION_PLACEHOLDER/$version/g" /tmp/alfred-password-gen/info.plist
+
+cd /tmp/alfred-password-gen
+
+zip -r archive.zip *
+
+cd "${script_dir}"/..
+mv /tmp/alfred-password-gen/archive.zip password-generator.alfredworkflow
